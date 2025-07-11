14a13b99ccc6a00a54cab0b5a43d8f4a58f1ce1a
u23ai105 <u23ai105@coed.svnit.ac.in>
2025-07-11 11:31:50 +0530

    dataset prep ✔

---
+++ img_dataset.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+from torch.utils.data.dataset import Dataset
+from torchvision import transforms
+
+class ImgDataset(Dataset):
+    def _init__(self,root_path,test=False):
+        self.root_path=root_path
+        if test:
+            self.images=sorted([os.path.join(root_path,i) for i in os.listdir(root_path)])
+            self.masks=sorted([os.path.join(root_path,"test_masks",i) for i in os.listdir(os.path.join(root_path,"test_masks"))])
+        else:
+            self.images=sorted([os.path.join(root_path,i) for i in os.listdir(root_path)])
+            self.masks=sorted([os.path.join(root_path,"train_masks",i) for i in os.listdir(os.path.join(root_path,"train_masks"))])
+
+            self.transform=transforms.Compose([
+                transforms.Resize((512,512)),
+                transforms.ToTensor()
+            ])
+
+    def _getitem_(self,index):
+        img=Image.open(self.images[index]).convert("RGB")
+        mask=Image.open(self.masks[index]).convert("L")
+
+        return self.transform(img),self.transform(mask)
+
+    def _len__(self):
+        return len(self.images)
---
+++ masking.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import numpy as np
+from pycocotools.coco import COCO
+from tqdm import tqdm
+
+def generate_and_save_masks(root_path, split_name, annotation_fname, output_mask_dir):
+    """
+    root_path: Root directory containing images and annotation
+    split_name: e.g. "train" or "test"
+    annotation_fname: COCO-style annotation file (e.g., "annotations.json")
+    output_mask_dir: Directory where the generated masks will be saved
+    """
+    # Setup paths
+    annotation_path = os.path.join(root_path, split_name, annotation_fname)
+    image_dir = os.path.join(root_path, split_name)
+    os.makedirs(output_mask_dir, exist_ok=True)
+
+    # Load COCO annotations
+    coco = COCO(annotation_path)
+    img_ids = coco.getImgIds()
+
+    for img_id in tqdm(img_ids, desc="Generating masks"):
+        img_info = coco.loadImgs(img_id)[0]
+        file_name = img_info["file_name"]
+        height, width = img_info["height"], img_info["width"]
+
+        # Create empty mask
+        mask = np.zeros((height, width), dtype=np.uint8)
+
+        # Load and combine annotations
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        anns = coco.loadAnns(ann_ids)
+        for ann in anns:
+            mask |= coco.annToMask(ann)
+
+        # Save mask as PNG
+        base_name = os.path.splitext(file_name)[0]
+        mask_path = os.path.join(output_mask_dir, base_name + ".png")
+        cv2.imwrite(mask_path, mask * 255)
+
+# Example usage
+generate_and_save_masks(
+    root_path="/Users/muzammilmohammad/Documents/CSAB/csab/Python/archive",
+    split_name="valid",
+    annotation_fname="annotations.json",
+    output_mask_dir="/Users/muzammilmohammad/Documents/CSAB/csab/Python/archive/valid/valid_masks"
+)
